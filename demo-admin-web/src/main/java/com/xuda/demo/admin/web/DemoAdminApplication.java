1e30967271832a572ca07eae96f007f1b4c7ef3a
xuda27 <xuda27@163.com>
2018-11-07 09:24:26 +0800

    first commit

---
+++ demo-admin-web/src/main/java/com/xuda/demo/admin/web/DemoAdminApplication.java
@@ -0,0 +1,12 @@
+package com.xuda.demo.admin.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAdminApplication.class, args);
+    }
+}
